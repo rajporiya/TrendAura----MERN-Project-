14d822d8212597b543447fd04527eb9c0996fc0e
rajporiya <rajpriya81@gmail.com>
2026-03-15 13:45:17 +0530

    admin dashboard

--- frontend/src/Admin/Dashboard.jsx
+++ frontend/src/Admin/Dashboard.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../AdminStyles/Dashboard.css'
 import Navbar from '../componant/Navbar'
 import PageTitle from '../componant/PageTitle'
 import Footer from '../componant/Footer'
-import { AddBox, AttachMoney, CheckCircle, Dashboard as DashboardIcon, Error, Instagram, Inventory, LinkedIn, People, Reviews, ShoppingCart, Star, YouTube   } from '@mui/icons-material'
+import { AddBox, AttachMoney, CheckCircle, CurrencyRupee, Dashboard as DashboardIcon, Error, Instagram, Inventory, LinkedIn, People, Reviews, ShoppingCart, Star, YouTube   } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAllOrders, fetchAllProducts } from '../feature/admin/adminSlice'
 
 export default function Dashboard() {
+    const {orders, products, totalAmount}= useSelector(state=>state.admin)
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(fetchAllProducts())
+        dispatch(fetchAllOrders())
+    }, [dispatch])
+
+    const totalProducts = products.length;
+    const totalOrders = orders.length;
+    const outOfStock = products.filter(product => product.stock === 0).length
+    const inStock = products.filter(product => product.stock > 0).length;
+    const totalReview = products.reduce((acc, product) => acc + (product.Reviews?.length || 0), 0)
   return (
     <>
     <Navbar />
@@ -43,32 +58,32 @@ export default function Dashboard() {
                 <div className="stat-box">
                     <Inventory className='icon'/>
                     <h3>Total Products</h3>
-                    <p>4</p>
+                    <p>{totalProducts}</p>
                 </div>
                 <div className="stat-box">
                     <ShoppingCart className='icon'/>
                     <h3>Total Orders</h3>
-                    <p>10</p>
+                    <p>{totalOrders}</p>
                 </div>
                 <div className="stat-box">
                     <Star className='icon'/>
                     <h3>Total Revies</h3>
-                    <p>10</p>
+                    <p>{totalReview}</p>
                 </div>
                 <div className="stat-box">
-                    <AttachMoney className='icon'/>
+                    <CurrencyRupee className='icon'/>
                     <h3>Total Revenue</h3>
-                    <p>10</p>
+                    <p>{totalAmount}</p>
                 </div>
                    <div className="stat-box">
                     <Error className='icon'/>
                     <h3>Out Of stock Revenue</h3>
-                    <p>10</p>
+                    <p>{outOfStock}</p>
                 </div>
                 <div className="stat-box">
                     <CheckCircle className='icon'/>
                     <h3>In  Of stock Revenue</h3>
-                    <p>10</p>
+                    <p>{inStock}</p>
                 </div>
             </div>
 
